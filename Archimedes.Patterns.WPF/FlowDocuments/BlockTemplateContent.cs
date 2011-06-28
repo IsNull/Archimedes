a5bc4847bdf8e806b5b06577fe01f3acf7ade020
pbu <isnull@securityvision.ch>
2011-06-28 13:27:13 +0200

    added binding support  for FlowDocument

---
+++ Archimedes.Patterns.WPF/FlowDocuments/BlockTemplateContent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Archimedes.Patterns.WPF.FlowDocuments
+{
+    public class BlockTemplateContent : Section
+    {
+        private static readonly DependencyProperty TemplateProperty = DependencyProperty.Register("Template", typeof(DataTemplate), typeof(BlockTemplateContent), new PropertyMetadata(OnTemplateChanged));
+
+        public DataTemplate Template {
+            get {
+                return (DataTemplate)GetValue(TemplateProperty);
+            }
+            set {
+                SetValue(TemplateProperty, value);
+            }
+        }
+
+
+        public BlockTemplateContent() {
+            Helpers.FixupDataContext(this);
+            Loaded += BlockTemplateContent_Loaded;
+        }
+
+
+        private void BlockTemplateContent_Loaded(object sender, RoutedEventArgs e) {
+            GenerateContent(Template);
+        }
+
+
+        private void GenerateContent(DataTemplate template) {
+            Blocks.Clear();
+            if (template != null) {
+                FrameworkContentElement element = Helpers.LoadDataTemplate(template);
+                Blocks.Add((Block)element);
+            }
+        }
+
+        private void OnTemplateChanged(DataTemplate dataTemplate) {
+            GenerateContent(dataTemplate);
+        }
+
+
+        private static void OnTemplateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            ((BlockTemplateContent)d).OnTemplateChanged((DataTemplate)e.NewValue);
+        }
+    }
+}
---
+++ Archimedes.Patterns.WPF/FlowDocuments/Fragment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+using System.Windows;
+
+namespace Archimedes.Patterns.WPF.FlowDocuments
+{
+    [ContentProperty("Content")]
+    public class Fragment : FrameworkElement
+    {
+        private static readonly DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(FrameworkContentElement), typeof(Fragment));
+
+        public FrameworkContentElement Content {
+            get {
+                return (FrameworkContentElement)GetValue(ContentProperty);
+            }
+            set {
+                SetValue(ContentProperty, value);
+            }
+        }
+    }
+}
