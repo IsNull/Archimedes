42ede3f2e2618a659a6f14d2be11e754ba2edafa
pbu <isnull@securityvision.ch>
2011-08-06 13:08:38 +0200

    added Drag n Drop Behavior to Archimedes WPF

---
+++ Archimedes.Patterns.WPF/DragNDrop/IDropTarget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Archimedes.Patterns.WPF.DragNDrop
+{
+    public interface IDropTarget
+    {
+        void DragOver(DropInfo dropInfo);
+        void Drop(DropInfo dropInfo);
+    }
+}
