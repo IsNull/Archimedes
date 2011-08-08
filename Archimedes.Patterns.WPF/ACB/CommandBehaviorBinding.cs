7d8d6897d7d8ee555657e11c2e2f0fc2589c673c
pbu <isnull@securityvision.ch>
2011-08-08 17:31:45 +0200

    ACB: avoid null ref exceptions

--- Archimedes.Patterns.WPF/ACB/CommandBehaviorBinding.cs
+++ Archimedes.Patterns.WPF/ACB/CommandBehaviorBinding.cs
@@ -96,7 +96,8 @@ namespace Archimedes.Patterns.WPF.ACB
         /// </summary>
         public void Execute()
         {
-            strategy.Execute(CommandParameter);
+            if (strategy != null)
+                strategy.Execute(CommandParameter);
         }
 
         #region IDisposable Members
