bf56ac41d15341507b7cbe88305189c3424cff5e
IsNull <isnull@securityvision.ch>
2013-01-21 11:06:00 +0100

    added Archimedes.Genetics - here comes the fun

---
+++ Archimedes.Genetics/Candidate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archimedes.Genetics
+{
+    /// <summary>
+    /// Represents a solution candidate
+    /// </summary>
+    public abstract class Candidate
+    {
+
+        public abstract double Fitness();
+
+
+    }
+}
