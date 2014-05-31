74cb2fd39732bb10496ad97e967d6c3a263a49c3
IsNull <isnull@securityvision.ch>
2014-05-31 11:54:07 +0200

    introduced SizeD

---
+++ Archimedes.Geometry/SizeD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archimedes.Geometry
+{
+    /// <summary>
+    /// Represents a size information. (Immutable)
+    /// </summary>
+    public struct SizeD
+    {
+
+        public static readonly SizeD Empty = new SizeD(0,0);
+
+        public readonly double Width;
+        public readonly double Height;
+
+        public SizeD(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public bool IsEmpty { get { return Width == 0 && Height == 0; } }
+    }
+}
