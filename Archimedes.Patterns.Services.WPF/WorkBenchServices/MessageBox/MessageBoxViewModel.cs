f93209189321ae6fb5702095502aec04a8dd64d8
IsNull <pascalbuettiker@bluewin.ch>
2015-03-26 14:31:11 +0100

    cleanup message box services

--- Archimedes.Patterns.Services.WPF/WorkBenchServices/MessageBox/MessageBoxViewModel.cs
+++ Archimedes.Patterns.Services.WPF/WorkBenchServices/MessageBox/MessageBoxViewModel.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using Archimedes.Patterns.WPF.ViewModels;
 using Archimedes.Patterns.WPF.Commands;
+using Archimedes.Patterns.WPF.ViewModels;
 using Archimedes.Services.WPF.Resources;
-
 
 namespace Archimedes.Services.WPF.WorkBenchServices.MessageBox
 {
